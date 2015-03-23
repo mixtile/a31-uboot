f10bce0e752aaae8654e32bc35af1a3ee0743a79
Han Pengfei <pengphei@sina.com>
2015-03-23 11:22:58 +0800

    [loftq]: update machine id

--- board/sunxi/sun8iw1/sun8iw1.c
+++ board/sunxi/sun8iw1/sun8iw1.c
@@ -54,7 +54,7 @@ DECLARE_GLOBAL_DATA_PTR;
 /* add board specific code here */
 int board_init(void)
 {
-	gd->bd->bi_arch_number = LINUX_MACHINE_ID;
+	gd->bd->bi_arch_number = 0;
 	gd->bd->bi_boot_params = (PHYS_SDRAM_1 + 0x100);
 	debug("board_init storage_type = %d\n",uboot_spare_head.boot_data.storage_type);
 
